191a37f1bafc82ace9cd1d063f16074e8c91b1de
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-18 20:30:33 +0900

    propertyの情報を新規作成する際に空欄でも新規作成できてしまっていたので

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,7 @@
 class Property < ApplicationRecord
+  validates :property_name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :note, presence: true
 end
